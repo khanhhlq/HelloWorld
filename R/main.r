b622bd2fd3c05898f183cc79afac68f4af88d030
Nguyen Hai Dang <ndang3399@gmail.com>
2021-10-18 23:17:46 +0700

    Add New Language - Add R Hello World Code

---
+++ R/main.r
@@ -0,0 +1,11 @@
+# We can use the print() function
+print("Hello World!")
+# Output: "Hello World!"
+
+# Quotes can be suppressed in the output
+print("Hello World!", quote = FALSE)
+# Output: Hello World!
+
+# If there are more than 1 item, we can concatenate using paste()
+print(paste("Hello", "World!"))
+# Output: "Hello World!"
