801e9c009fccf36a06fedd267ad9e2c95c24a561
DuongDev <vietnamcf36@gmail.com>
2021-10-18 22:31:17 -0700

    Update HelloWorld with some type to print HelloWorld
    We updated file HelloWorld.js

---
+++ Javascript/HelloWorld.js
@@ -0,0 +1,12 @@
+// We have 3 types to print "HelloWorld" in Javascript
+
+// The first is function console.log() in JS
+console.log("HelloWorld");
+
+// The second is function alert() in JS
+alert("HelloWorld");
+
+// The first is function document.write() in JS
+document.write("HelloWorld");
+
+// The End. GoodLuck with some experience
